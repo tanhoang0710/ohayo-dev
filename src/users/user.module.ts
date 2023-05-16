e3ed0958a183eb09676c06b4836b9f5ee411e829
tanhun <tanhoang0710@gmail.com>
2023-05-16 09:15:41 +0700

    6 - Custom provider, useValue

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { UserMockService } from './user-mock.service';
+import { StoreConfig } from 'src/store/store.config';
+
+const configFB = {
+  appId: 'fb001',
+  appSecret: '123',
+};
 
 @Module({
   controllers: [UserController],
   providers: [
     {
       provide: UserService,
-      useClass: UserMockService,
+      useClass: UserService,
+    },
+    {
+      provide: 'APP_FACEBOOK',
+      useValue: configFB,
+    },
+    {
+      provide: 'STORE_CONFIG',
+      useValue: {
+        dir: 'store',
+        path: 'user',
+      } as StoreConfig,
     },
   ],
 })
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { UserDto } from './user.dto';
+import { StoreConfig } from 'src/store/store.config';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @Inject('APP_FACEBOOK') appFacebook: any,
+    @Inject('STORE_CONFIG') storeConfig: StoreConfig,
+  ) {
+    console.log(
+      '🚀 ~ file: user.service.ts:11 ~ UserService ~ storeConfig:',
+      storeConfig,
+    );
+    console.log(appFacebook);
+  }
+
   create(user): UserDto {
     return new UserDto();
   }
